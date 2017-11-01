dca89bd5ab0393adfc33c14625d85742ff02f4bd
CerealKill <2923413+cerealkill@users.noreply.github.com>
2017-11-01 15:34:46 +0100

    Restarting the services

--- updater.sh
+++ updater.sh
@@ -30,10 +30,10 @@ done
 # cp -v ./skel/eth-netstats.json ../authority_node/monitor/app.json
 # cp -v ./skel/authority.yml ../authority_node/docker-compose.yml
 
-MINER=$(tail -n 1 ../authority_node/config/authority.toml)
-cp ./config/authority.toml ../authority_node/config/authority.toml
-echo "${MINER}" >> ../authority_node/config/authority.toml
-cp ./config/chain.json ../authority_node/config/chain.json
+# MINER=$(tail -n 1 ../authority_node/config/authority.toml)
+# cp ./config/authority.toml ../authority_node/config/authority.toml
+# echo "${MINER}" >> ../authority_node/config/authority.toml
+# cp ./config/chain.json ../authority_node/config/chain.json
 sudo systemctl restart ewf-tobalaba-authority@ewf.service
 
 
