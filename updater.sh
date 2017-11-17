29eee79b9817cbebbb4177557c436ff4dbe4e466
ewf-updater <ewf-updater@ewf.org>
2017-11-17 11:36:31 +0100

    remove gas_floor_target

--- updater.sh
+++ updater.sh
@@ -56,9 +56,9 @@ done
 # sudo systemctl start ewf-tobalaba-authority@ewf.service
 
 # --- Change gas back to 8 mill
-# MINER=$(tail -n 1 ../authority_node/config/authority.toml)
-# cp ./config/authority.toml ../authority_node/config/authority.toml
-# echo "${MINER}" >> ../authority_node/config/authority.toml
+MINER=$(tail -n 1 ../authority_node/config/authority.toml)
+cp ./config/authority.toml ../authority_node/config/authority.toml
+echo "${MINER}" >> ../authority_node/config/authority.toml
 sudo systemctl stop ewf-tobalaba-authority@ewf.service
 sudo systemctl start ewf-tobalaba-authority@ewf.service
 
