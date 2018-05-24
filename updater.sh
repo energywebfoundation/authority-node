ff36b4228046b439f046a365184324e50cb95e14
ewf-updater <ewf-updater@ewf.org>
2018-05-24 11:04:01 +0200

    Parity revert

--- updater.sh
+++ updater.sh
@@ -169,7 +169,7 @@ done
      sudo systemctl start ewf-tobalaba-authority@ewf.service
 fi
 
- if grep 'SP' ../authority_node/monitor/app.json
+ if grep 'Parity' ../authority_node/monitor/app.json
  then
      sudo systemctl stop ewf-tobalaba-authority@ewf.service
      docker pull parity/parity:stable
