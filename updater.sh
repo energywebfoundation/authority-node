0d95ca7eca671b6d91d37e56e4d4500982c81076
ewf-updater <ewf-updater@ewf.org>
2018-06-04 21:26:20 +0200

    oli

--- updater.sh
+++ updater.sh
@@ -166,8 +166,8 @@ done
  if grep 'Stedin' ../authority_node/monitor/app.json
  then
      sudo systemctl stop ewf-tobalaba-authority@ewf.service
-     docker pull parity/parity:v1.9.3
-     cp ./skel/authority.yml ../authority_node/docker-compose.yml
+     #docker pull parity/parity:v1.9.3
+     #cp ./skel/authority.yml ../authority_node/docker-compose.yml
      sudo systemctl start ewf-tobalaba-authority@ewf.service
 fi
 
